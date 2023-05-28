7eb7bdc652821303f6bd6404330d7c7b2e8b73c3
bouraima <bouraima799@gmail.com>
2023-05-28 04:56:32 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,35 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Declarations
+        Scanner input= new Scanner(System.in);
+        float sec, hours2,hours3, min1,min,min2,min3, sec2,sec3;
+        int hours,seconds,minutes, seconds2;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+//Inputs
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Enter an amount of seconds:");
+        sec= input.nextFloat();
+
+//Process
+
+        seconds= (int) sec;
+        min1= sec/60; //total amount of minutes
+        hours2= min1/60;// total amount of hours
+        hours= (int)(hours2); //  actual amount of hours
+        hours3= (float)hours2-hours; // remaining hours
+        min3= hours3*60;//converts number of partial remaining hours to whole minutes
+        min= (int)min3; // gives the actual remaining minutes
+        min2= min3 - min; //  gives remaining seconds in minutes format
+        sec3=min2*60; //  gives actual remaining seconds in seconds format
+        sec2=(int)(sec3); //remaining seconds without decimals
+        minutes= (int)min;
+        seconds2=(int)sec2;
+
+//Outputs
+
+        System.out.println("You entered "+seconds+" seconds.");// amount of seconds that was entered
+        System.out.println(+seconds+" would be "+hours+" hours, "+minutes+" minutes and "+seconds2+" seconds.");
+
     }
 }
